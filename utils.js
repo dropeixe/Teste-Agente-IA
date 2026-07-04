7afb1097a4985be148fc01258d0ca40e7c376884
dropeixe <pedro.lima@idealsales.com.br>
2026-07-04 00:16:33 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,67 @@
+const fs = require("fs");
 
+function log(mensagem) {
+
+    const agora = new Date().toLocaleTimeString("pt-BR");
+
+    console.log(`[${agora}] ${mensagem}`);
+
+}
+
+async function esperar(ms){
+
+    return new Promise(resolve=>setTimeout(resolve,ms));
+
+}
+
+async function screenshot(page,nome){
+
+    try{
+
+        await page.screenshot({
+
+            path:`screenshots/${nome}.png`,
+
+            fullPage:true
+
+        });
+
+        log(`Screenshot salva: ${nome}.png`);
+
+    }
+
+    catch(e){
+
+        log(`Erro ao salvar screenshot: ${e.message}`);
+
+    }
+
+}
+
+function salvarTexto(nome,conteudo){
+
+    fs.writeFileSync(
+
+        `logs/${nome}`,
+
+        conteudo,
+
+        "utf8"
+
+    );
+
+    log(`${nome} salvo.`);
+
+}
+
+module.exports={
+
+    log,
+
+    esperar,
+
+    screenshot,
+
+    salvarTexto
+
+};
